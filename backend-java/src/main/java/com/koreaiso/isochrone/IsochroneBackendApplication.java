e9f3c5c5225b99d59380598339b61f96118007e2
Jin <ideasidus@gmail.com>
2025-11-17 15:31:53 +0000

    init: backend-java 초기설정

---
+++ backend-java/src/main/java/com/koreaiso/isochrone/IsochroneBackendApplication.java
@@ -0,0 +1,12 @@
+package com.koreaiso.isochrone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsochroneBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsochroneBackendApplication.class, args);
+    }
+}
---
+++ backend-java/src/test/java/com/koreaiso/isochrone/IsochroneBackendApplicationTests.java
@@ -0,0 +1,12 @@
+package com.koreaiso.isochrone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsochroneBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
